c71039679e191731e4ea1e997ac4e98a8d21cead
AbdullahTaher93 <abdullahtaher@correo.ugr.es>
2020-04-06 01:29:03 +0200

    books

---
+++ P1/QuickViewDemo/build/CMakeFiles/QuickViewDemo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QuickViewDemo.dir/QuickViewDemo.cxx.o"
+  "QuickViewDemo"
+  "QuickViewDemo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QuickViewDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ P1/ShrinkImageFilter/build/CMakeFiles/ShrinkImageFilter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ShrinkImageFilter.dir/ShrinkImageFilter.cxx.o"
+  "ShrinkImageFilter"
+  "ShrinkImageFilter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ShrinkImageFilter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
